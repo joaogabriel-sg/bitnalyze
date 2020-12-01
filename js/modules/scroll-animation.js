efd174cb749df94f235002bd7387ccadc9a941c4
João Gabriel <joaogabrielsg.00@gmail.com>
2020-12-01 10:24:17 -0300

    Scroll animation completed

---
+++ js/modules/scroll-animation.js
@@ -0,0 +1,35 @@
+import debounce from './debounce.js';
+
+export default class ScrollAnimation {
+  constructor(elements) {
+    this.elements = [...document.querySelectorAll(elements)];
+    this.halfWindow = window.innerHeight * 0.85;
+    this.checkDistances = debounce(this.checkDistances.bind(this), 30);
+  }
+
+  getElementsInformations() {
+    this.distances = this.elements.map((element) => {
+      const offset = element.offsetTop;
+      return { element, offset: Math.round(offset - this.halfWindow) };
+    });
+  }
+
+  checkDistances() {
+    this.distances.forEach(({ element, offset }) => {
+      if (window.pageYOffset > offset) {
+        element.classList.add('active');
+      } else if (element.classList.contains('active')) {
+        element.classList.remove('active');
+      }
+    });
+  }
+
+  init() {
+    if (this.elements.length) {
+      this.getElementsInformations();
+      this.checkDistances();
+      window.addEventListener('scroll', this.checkDistances);
+    }
+    return this;
+  }
+}
--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 import MenuMobile from './modules/menu-mobile.js';
+import ScrollAnimation from './modules/scroll-animation.js';
 
 const menuMobile = new MenuMobile('.header__mobile-burger');
 menuMobile.init();
+
+const scrollAnimation = new ScrollAnimation('[data-anime="scroll"]');
+scrollAnimation.init();
