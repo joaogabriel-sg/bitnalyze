4ab101736396a8b13331d935eadf7c0efaf8fc09
João Gabriel <joaogabrielsg.00@gmail.com>
2020-12-02 21:25:07 -0300

    Fetch Bitcoin API completed and inserted BTC value in some fields

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ import MenuMobile from './modules/menu-mobile.js';
 import ScrollAnimation from './modules/scroll-animation.js';
 import AccordionList from './modules/accordion-list.js';
 import SmoothScroll from './modules/smooth-scroll.js';
+import BitcoinDatas from './modules/fetch-btc.js';
 
 const menuMobile = new MenuMobile('.header__mobile-burger');
 menuMobile.init();
@@ -14,3 +15,9 @@ accordionList.init();
 
 const smoothScroll = new SmoothScroll('a[href^="#"]');
 smoothScroll.init();
+
+const bitcoinDatas = new BitcoinDatas({
+  now: '.price__btc-now span',
+  forHundred: '.price__btc-hundred span',
+});
+bitcoinDatas.init();
